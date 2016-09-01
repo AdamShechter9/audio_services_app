ef8226e8bb548b9b6039101417be32b69999bb62
Adam Shechter <adam.s.develop@gmail.com>
2016-09-01 11:03:57 -0700

    animation added to front page with quotes.  Footer on index page modified.

--- client/js/init.js
+++ client/js/init.js
@@ -5,6 +5,15 @@
     $('.parallax').parallax();
     $('.modal-trigger').leanModal();
     $('select').material_select();
+     	var options = [
+	    	{selector: '#quotes-list-stagger', offset: 300, callback: function(el) {
+	      		Materialize.showStaggeredList($(el));
+	    	} },
+	    	{selector: '#quotes-list-stagger2', offset: 300, callback: function(el) {
+	      		Materialize.showStaggeredList($(el));
+	    	} }
+		];
+	Materialize.scrollFire(options);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
