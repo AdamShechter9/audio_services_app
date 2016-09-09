b1206435a9de7246f2ab939a27bad1d19ea47674
Adam Shechter <adam.s.develop@gmail.com>
2016-09-09 00:52:26 -0700

    Angular Partials loaded. Single Page Application implemented.

--- client/js/app-angular.js
+++ client/js/app-angular.js
@@ -2,14 +2,35 @@
 // Warp9 Mixing and Mastering Services
 // adam.s.develop@gmail.com
 
-var TopApp = angular.module('TopApp', ['ui.materialize']);
+var TopApp = angular.module('TopApp', ['ui.materialize', 'ngRoute']);
 
 var ADMIN = "warp9mixmaster@gmail.com";
 var ADMINTITLE = "Warp9 Audio";
 
 
 // -----------------------------------------------------------------
-// DIRECTIVES
+// CONFIG
+TopApp.config( function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: "/partials/home.html"
+		})
+		.when('/about', {
+			templateUrl: "/partials/about.html"
+		})
+		.when('/contact', {
+			templateUrl: "/partials/contact.html"
+		})
+		.when('/uploadfile', {
+			templateUrl: "/partials/uploadfile.html"
+		})
+		.when('/inbox', {
+			templateUrl: "/partials/inbox.html"
+		})
+		.otherwise({
+			redirectTo: "/"
+		});
+})
 
 
 
