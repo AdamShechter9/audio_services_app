b475ccc5c89e31b4cf8785e2923cbe3fee2a1e46
Adam Shechter <adam.s.develop@gmail.com>
2016-11-15 00:49:23 -0800

    fixed errors in upload file.

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -116,8 +116,10 @@ module.exports = {
 		console.log("upload a file from ", req.session.name);
         // create an incoming form object
         var form = new formidable.IncomingForm();
+				var fileList = [];
         // specify that we want to allow the user to upload multiple files in a single request
         form.multiples = true;
+				form.type = true;
         // store all uploads in the /uploads/userid directory
         // make one if directory doesn't exist
         if(!fs.existsSync(path.join(__dirname, '../../client/uploads/' + req.session.userid))){
@@ -131,8 +133,10 @@ module.exports = {
         form.uploadDir = path.join(__dirname, '../../client/uploads/' + req.session.userid);
         // every time a file has been uploaded successfully,
         // rename it to it's original name
+
         form.on('file', function(field, file) {
             fs.rename(file.path, path.join(form.uploadDir, file.name));
+						fileList.push(file.name);
         });
         // log any errors that occur
         form.on('error', function(err) {
@@ -140,9 +144,10 @@ module.exports = {
         });
         // once all the files have been uploaded, send a response to the client
         form.on('end', function() {
+						var fileListParsed = fileList.join("\n");
             var newMessage = new Message();
-            newMessage.title = "New Upload from "+req.session.name;
-            newMessage.text = "A new upload was made by " + req.session.name + " for " + ADMINTITLE + ".";
+            newMessage.title = "[www.warp9audio.co] "+"New Upload from "+req.session.name;
+            newMessage.text = "A new upload was made by " + req.session.name + " for " + ADMINTITLE + ".\n\nFILES:\n"+fileListParsed;
             newMessage.name = req.session.name;
             newMessage.email = req.session.email;
             newMessage.read = false;
